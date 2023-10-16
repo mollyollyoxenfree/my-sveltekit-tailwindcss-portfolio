2217e871cb9dcf1e3d8ed2b119189c6cfd71e057
Molly Smith <molly.ivester@gmail.com>
2023-10-16 13:18:56 -0400

    deploy config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
 <script>
     import Main from "../components/Main.svelte";
 
-    /** @type {import('@sveltejs/adapter-vercel').Config} */
-export const config = {
-	runtime: 'nodejs18.x'
-};
-
 </script>
 
 <Main />
