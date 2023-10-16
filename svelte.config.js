f9858af8adb8679c54115acbde91b18ec4025ab4
Molly Smith <molly.ivester@gmail.com>
2023-10-16 15:30:35 -0400

    config deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// const config = {
+//   kit: {
+//     adapter: adapter(),
+//   },
+//   preprocess: vitePreprocess(),
+// };
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter(),
+    paths: {
+      base:
+        process.env.NODE_ENV === 'production'
+          ? '/my-sveltekit-tailwindcss-portfolio'
+          : '',
+    },
   },
-  preprocess: vitePreprocess(),
 };
+
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-// import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
-
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: './docs', // relative to index.html
-    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-  },
+  plugins: [sveltekit()],
 });
