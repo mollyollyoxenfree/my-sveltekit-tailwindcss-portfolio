f6f2747cf3d2a0af53ff81c86fc45d8d4b5141e9
Molly Smith <molly.ivester@gmail.com>
2023-10-16 13:37:21 -0400

    deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-vercel';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
@@ -8,6 +8,6 @@ const config = {
       runtime: 'nodejs18.x',
     }),
   },
-  // preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 };
 export default config;
