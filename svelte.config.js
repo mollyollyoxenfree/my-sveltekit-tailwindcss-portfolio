803c0fffbdbbe46f9b7dbdf9651e91a83fca0045
Molly Smith <molly.ivester@gmail.com>
2023-10-17 11:01:22 -0400

    config deploy

--- svelte.config.js
+++ svelte.config.js
@@ -9,27 +9,43 @@
 //   preprocess: vitePreprocess(),
 // };
 // export default config;
+////////////////////////////////////////////////////
 
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// for more information about preprocessors
+// preprocess: vitePreprocess(),
+
+// kit: {
+//   adapter: adapter({
+//     fallback: '200.html', // may differ from host to host
+//     }),
+//     paths: {
+//       base:
+//         process.env.NODE_ENV === 'production'
+//           ? '/my-sveltekit-tailwindcss-portfolio'
+//           : '',
+//     },
+//   },
+// };
+
+// export default config;
+/////////////////////////////////////////////////////
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter({
-      fallback: '200.html', // may differ from host to host
-    }),
-    paths: {
-      base:
-        process.env.NODE_ENV === 'production'
-          ? '/my-sveltekit-tailwindcss-portfolio'
-          : '',
-    },
+    adapter: adapter(),
   },
+};
+
+config.paths = {
+  base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 };
 
 export default config;
