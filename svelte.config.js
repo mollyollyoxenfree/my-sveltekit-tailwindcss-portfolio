72870333e7629a17719e213358e6e88d644ac79a
Molly Smith <molly.ivester@gmail.com>
2023-10-17 11:30:50 -0400

    config deploy

--- svelte.config.js
+++ svelte.config.js
@@ -25,7 +25,7 @@ const config = {
     paths: {
       base:
         process.env.NODE_ENV === 'production'
-          ? '/mollyollyoxenfree/my-sveltekit-tailwindcss-portfolio/'
+          ? '/my-sveltekit-tailwindcss-portfolio'
           : '',
     },
   },
