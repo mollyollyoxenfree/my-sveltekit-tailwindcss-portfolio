0b76d159a7c4d381603868418531e888d098fc4e
Molly Smith <molly.ivester@gmail.com>
2023-10-18 11:30:00 -0400

    config deploy

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -15,11 +15,7 @@ let benefits = [
 
 </script>
 
-<style global>
-  @import 'tailwindcss/base.css';
-  @import 'tailwindcss/components.css';
-  @import 'tailwindcss/utilities.css';
-</style>
+
 
 <main class="flex flex-col flex-1 p-4">
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
