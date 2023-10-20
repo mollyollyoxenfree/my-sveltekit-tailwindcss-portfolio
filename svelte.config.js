8a258c73248aefc1e9e725c87a6c9f202994ccde
Molly Smith <molly.ivester@gmail.com>
2023-10-20 11:52:22 -0400

    config deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,25 @@
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// const dev = process.argv.includes('dev');
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   preprocess: vitePreprocess(),
+//   kit: {
+//     adapter: adapter(),
+//     paths: {
+//       // base: dev ? '' : process.env.BASE_PATH,
+//       base:
+//         process.env.NODE_ENV === 'production'
+//           ? '/my-sveltekit-tailwindcss-portfolio'
+//           : '',
+//     },
+//   },
+// };
+
+// export default config;
+//////////////////////////////////////////////////////////////
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -9,14 +31,10 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      // base: dev ? '' : process.env.BASE_PATH,
-      base:
-        process.env.NODE_ENV === 'production'
-          ? '/my-sveltekit-tailwindcss-portfolio'
-          : '',
+      base: '/my-sveltekit-tailwindcss-portfolio',
+      assets: '/my-sveltekit-tailwindcss-portfolio',
     },
   },
 };
 
 export default config;
-//////////////////////////////////////////////////////////////
