f7c40ad82677c8b3a7d7704c955186179cc82a99
Molly Smith <molly.ivester@gmail.com>
2023-10-20 11:24:23 -0400

    config deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,38 +1,3 @@
-// import adapter from '@sveltejs/adapter-auto';
-
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// const config = {
-//   kit: {
-//     adapter: adapter(),
-//   },
-//   preprocess: vitePreprocess(),
-// };
-// export default config;
-////////////////////////////////////////////////////
-
-// import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   preprocess: vitePreprocess(),
-
-//   kit: {
-//     adapter: adapter({
-//       fallback: 'index.html',
-//     }),
-//     paths: {
-// base:
-//   process.env.NODE_ENV === 'production'
-//     ? '/my-sveltekit-tailwindcss-portfolio'
-//     : '',
-//     },
-//   },
-// };
-
-// export default config;
-/////////////////////////////////////////////////////
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -55,21 +20,3 @@ const config = {
 
 export default config;
 //////////////////////////////////////////////////////////////
-// import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// const dev = precess.argv.includes('dev');
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   preprocess: vitePreprocess(),
-
-//   kit: {
-//     adapter: adapter(),
-//     paths: {
-//       base: dev ? '' : <process className="env BASE_PATH"></process>,
-//     },
-//   },
-// };
-
-// export default config;
